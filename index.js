80b6c8e6f32ceca2ee150e4f2adc61fc9e2aa385
Antoniel Bordin <antonielbordin@hotmail.com>
2020-11-23 20:48:08 -0300

    Create RouteObserver

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import Router from './src/Router.svelte';
 import Route from './src/Route.svelte';
 import NotFound from './src/NotFound.svelte';
+import RouteObserver from './src/RouteObserver.svelte';
 
 export {
   Route,
   Router,
-  NotFound
+  NotFound,
+  RouteObserver
 }
