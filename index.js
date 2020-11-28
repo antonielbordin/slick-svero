06082bc5bdd98e5f6fc5a092f7b2786c8f018d5c
Antoniel Bordin <antonielbordin@hotmail.com>
2020-11-28 12:01:26 -0300

    Adjustment in export options

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import Router from './src/Router.svelte';
 import Route from './src/Route.svelte';
 import NotFound from './src/NotFound.svelte';
 
-export default {
+export {
   Route,
   Router,
   NotFound
